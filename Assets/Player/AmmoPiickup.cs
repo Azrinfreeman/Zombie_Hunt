9d06a906d6497ba022b662a8bb7b3e244f92f16f
Azrinfreeman <azrinfreeman2@gmail.com>
2021-11-30 02:32:54 +0800

    UI Updated
    -Crosshair has been updated with circle and dot instead of just dot in the center.
    
    -Details of item will be shown on the bottom left of the screen when player picked the item

--- Assets/Player/AmmoPiickup.cs
+++ Assets/Player/AmmoPiickup.cs
@@ -1,32 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class AmmoPiickup : MonoBehaviour
 {
+   
+    public UIController ui;
     public PlayerController player;
+    
+
     public bool collected;
 
     public bool mm9Box;
     public bool mm7Box;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player" && !collected && mm7Box)
         {
             player.activeGun.GetAmmo7();
             Destroy(gameObject);
+            ui.detailCollected.text = "+30 "+"AK ammo collected";
+            ui.uiAnim.SetTrigger("isFading");
             collected = true;
         }
         if (other.tag == "Player" && !collected && mm9Box)
         {
             player.activeGun.GetAmmo9();
             Destroy(gameObject);
+            ui.detailCollected.text = "+15 " + "Pistol ammo collected";
+            ui.uiAnim.SetTrigger("isFading");
             collected = true;
         }
     }
     void Start()
     {
+        ui = GameObject.Find("Canvas").GetComponent<UIController>();
         player = GameObject.Find("player").GetComponent<PlayerController>();
+       
+       
     }
 
     // Update is called once per frame
@@ -42,14 +56,30 @@ public class AmmoPiickup : MonoBehaviour
 
             player.activeGun.GetAmmo7();
             Destroy(gameObject);
+            ui.detailCollected.text += "+30 " + "AK ammo collected\n";
+           
+            ui.uiAnim.SetTrigger("isFading");
+            
             collected = true;
+            
         }
         else if (mm9Box)
         {
 
             player.activeGun.GetAmmo9();
             Destroy(gameObject);
+            ui.detailCollected.text += "+15 " + "Pistol ammo collected\n";
+           
+            ui.uiAnim.SetTrigger("isFading");
+            
             collected = true;
+           
         }
+       
+        
     }
+
+   
+
+    
 }
--- Assets/Player/UIController.cs
+++ Assets/Player/UIController.cs
@@ -2,19 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class UIController : MonoBehaviour
 {
     public static UIController instance;
 
     public Text ammoText;
+    public TextMeshProUGUI detailCollected;
+    public Animator uiAnim;
     void Start()
     {
-        
+       
+        detailCollected = GameObject.Find("collectedDetails").GetComponent<TextMeshProUGUI>();
+        uiAnim = GameObject.Find("collectedDetails").GetComponent<Animator>();
+        uiAnim.SetTrigger("isFading");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (uiAnim.GetCurrentAnimatorStateInfo(0).IsName("UIDetails"))
+        {
+            detailCollected.text = "";
+
+        }
     }
+
+    
+  
 }
